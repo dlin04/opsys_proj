6cb67b9b527f420c534443d2143d78cd914cce05
dlin04 <derricklin22xc@gmail.com>
2024-07-20 17:59:35 -0400

    define arguments

--- project.c
+++ project.c
@@ -2,5 +2,11 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv){
+    int n = atoi(*(argv + 1)); // number of processes to simulate
+    int n_cpu = atoi(*(argv + 2)); // number of processes cpu bound
+    int seed = atoi(*(argv + 3)); // pseduo-random number sequence seed
+    int lambda = atoi(*(argv + 4)); // interarrival times
+    int threshold = atoi(*(argv + 5)); // upper bound
+
     return EXIT_SUCCESS;
 }
