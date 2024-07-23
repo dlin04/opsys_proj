3acf89afde932754028bbf92803a3ed75b3bb21e
dannyzou18 <dzou3814@gmail.com>
2024-07-22 20:23:01 -0400

    function to print start

--- project.cpp
+++ project.cpp
@@ -1,5 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void print_start(int n, int n_cpu, int seed, double lambda, int threshold) {
+    cout << "<<< PROJECT PART I" << endl;
+    if (n_cpu > 1) {
+        cout << "<<< -- process set (n=" << n << ") with " << n_cpu << " CPU-bound processes" << endl;
+    } else {
+        cout << "<<< -- process set (n=" << n << ") with " << n_cpu << " CPU-bound process" << endl;
+    }
+    cout << "<<< -- seed=" << seed << "; lambda=" << fixed << setprecision(6) << lambda << "; bound=" << threshold << endl;
+}
 
 int main(int argc, char **argv){
     if (argc != 6) {
@@ -13,26 +27,22 @@ int main(int argc, char **argv){
     double lambda = atof(*(argv + 4)); // interarrival times
     int threshold = atoi(*(argv + 5)); // upper bound
 
-    printf("n = %d\n", n);
-    printf("n_cpu = %d\n", n_cpu);
-    printf("seed = %d\n", seed);
-    printf("lambda = %f\n", lambda);
-    printf("threshold = %d\n", threshold);
-
     // Check if n, n_cpu, seed, threshold are integers
     if (n != static_cast<int>(n) || n_cpu != static_cast<int>(n_cpu) || seed != static_cast<int>(seed) || threshold != static_cast<int>(threshold)) {
-        fprintf(stderr, "ERROR: Invalid argument(s)\n");
+        cerr << "ERROR: Invalid argument(s)" << endl;
         return EXIT_FAILURE;
     }
     // Check if lambda is float
     if (lambda != static_cast<double>(lambda)) {
-        fprintf(stderr, "ERROR: Invalid argument(s)\n");
+        cerr << "ERROR: Invalid argument(s)" << endl;
         return EXIT_FAILURE;
     }
     if (n <= 0 || n_cpu <= 0 || seed <= 0 || lambda <= 0 || threshold <= 0) {
-        fprintf(stderr, "ERROR: Invalid argument(s)\n");
+        cerr << "ERROR: Invalid argument(s)" << endl;
         return EXIT_FAILURE;
     }
+
+    print_start(n, n_cpu, seed, lambda, threshold);
 
     return EXIT_SUCCESS;
 }
